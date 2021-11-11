afd812bea2bac7367e05a16ed88bbaf29f8c0cc7
LeynaD0 <danielsmd521@gmail.com>
2021-11-11 18:46:16 +0000

    Cambios
    Luces, sonidos y efectos

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioControl: MonoBehaviour
+{
+    public static AudioControl instance;
+
+    public AudioSource musicSource;
+    public AudioSource effectsSource;
+    public Slider sliderMusic;
+    public Slider sliderEffects;
+
+    public void Awake()
+    {
+        instance = this;
+     
+        PlayerPrefs.SetFloat("volumenMusica", 1f);
+        PlayerPrefs.SetFloat("volumenEfectos", 1f);
+        MenuVolume();
+
+    }
+
+    private void MenuVolume()
+    {
+        musicSource.volume = PlayerPrefs.GetFloat("volumenMusica");
+        effectsSource.volume = PlayerPrefs.GetFloat("volumenEfectos");
+    }
+
+    public void SliderMusicModified()
+    {
+        musicSource.volume = sliderMusic.value;
+    }
+
+    public void SliderEffectsModified()
+    {
+        effectsSource.volume = sliderEffects.value;
+    }
+}
