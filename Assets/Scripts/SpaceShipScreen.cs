aa83fc62d6b991ed621640705940ea90c2ca3770
LeynaD0 <danielsmd521@gmail.com>
2021-12-16 18:06:21 +0000

    Cambios
    Agregado de cambios de nave y diferentes naves

--- Assets/Scripts/SpaceShipScreen.cs
+++ Assets/Scripts/SpaceShipScreen.cs
@@ -15,27 +15,44 @@ public class SpaceShipScreen : MonoBehaviour
     public int index = 0;
 
     public GameObject[] spaceShips;
-    
+
     private float speed = 1.0f;
-    
-    
+
+
     void Start()
     {
         Debug.Log(infoSpaceShip[index].speed);
         Debug.Log(infoSpaceShip[index].shield);
         Debug.Log(infoSpaceShip[index].heat);
         Debug.Log(infoSpaceShip[index].spaceshipName);
+
+        for (int i = 0; i < spaceShips.Length; i++)
+        {
+            spaceShips[i].SetActive(true);
+
+            if (i == index)
+            {
+                spaceShips[i].SetActive(true);
+            }
+
+            else
+            {
+                spaceShips[i].SetActive(false);
+            }
+
+            nameShip.text = infoSpaceShip[index].spaceshipName;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (speedSlider.value < infoSpaceShip[index].speed)
+        if (speedSlider.value < infoSpaceShip[index].speed)
         {
             speedSlider.value += Time.deltaTime * speed;
         }
 
-       if(shieldSlider.value < infoSpaceShip[index].shield)
+        if (shieldSlider.value < infoSpaceShip[index].shield)
         {
             shieldSlider.value += Time.deltaTime * speed;
         }
@@ -46,11 +63,61 @@ public class SpaceShipScreen : MonoBehaviour
         }
     }
 
-    public void ReturnData()
+    public void NextShip()
     {
-        
-         speedSlider.value = 0.0f;
-         shieldSlider.value = 0.0f;
-         heatSlider.value = 0.0f;
+        index++;
+
+        if (index > 2)
+        {
+            index = 0;
+        }
+        speedSlider.value = 0.0f;
+        shieldSlider.value = 0.0f;
+        heatSlider.value = 0.0f;
+        nameShip.text = infoSpaceShip[index].spaceshipName;
+
+        for (int i = 0; i < spaceShips.Length; i++)
+        {
+            spaceShips[i].SetActive(true);
+
+            if (i == index)
+            {
+                spaceShips[i].SetActive(true);
+            }
+
+            else
+            {
+                spaceShips[i].SetActive(false);
+            }
+        }
+    }
+
+    public void PreviusShip()
+    {
+        index--;
+
+        if (index > 2)
+        {
+            index = 0;
+        }
+        speedSlider.value = 0.0f;
+        shieldSlider.value = 0.0f;
+        heatSlider.value = 0.0f;
+        nameShip.text = infoSpaceShip[index].spaceshipName;
+
+        for (int i = 0; i < spaceShips.Length; i++)
+        {
+            spaceShips[i].SetActive(true);
+
+            if (i == index)
+            {
+                spaceShips[i].SetActive(true);
+            }
+
+            else
+            {
+                spaceShips[i].SetActive(false);
+            }
+        }
     }
 }
